d0dfce35ed1508ffb5e0eec40ecb98c15310b86c
Mr.Miss <keselekpermen69@gmail.com>
2020-10-03 17:21:02 +0000

    pylint: auto fixes

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -12,8 +12,7 @@ from random import randint
 
 from telethon.events import StopPropagation
 
-from userbot import (AFKREASON, COUNT_MSG, CMD_HELP, ISAFK, BOTLOG,
-                     BOTLOG_CHATID, USERS, PM_AUTO_BAN)
+from userbot import (AFKREASON, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN)
 from userbot.events import register
 
 USER_AFK = {}
